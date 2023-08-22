da50ab4e79e73ca5fb54ae0a2780646b67e90900
Balazs Radvanyi <balazs@riltech.co>
2023-08-22 17:50:02 +0200

    feat(index-page): implemented navigation between avatar picking and the game screen

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,32 @@
+import { useRouter } from "next/router";
 import { useState } from "react";
 
 const Home = () => {
   const [ username, setUsername ] = useState('');
+  const router = useRouter();
 
   const onChange = (event) => {
     setUsername(event.target.value);
   };
 
-  const handleClick = (event) => {
-    alert(`picked username: ${username}`);
+  const handleClick = async () => {
+    try {
+      const res = await fetch(`http://localhost:3000/api/createPlayer`, {
+        method: 'POST',
+        body: JSON.stringify({
+          data:{name:username}, 
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        }
+      });
+
+      const data = await res.json();
+
+      router.push({pathname: '/game', query: { name: data.name, money: data.money}})
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   return <div>
