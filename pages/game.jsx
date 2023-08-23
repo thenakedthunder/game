5e75f5635840ff6d353031aad1f29ddfbff45aee
Balazs Radvanyi <balazs@riltech.co>
2023-08-23 15:04:06 +0200

    refactor(betting-page): made some refinement in betting logic

--- pages/game.jsx
+++ pages/game.jsx
@@ -3,8 +3,10 @@ import { useState } from 'react';
 
 const Game = () => {
   const router = useRouter();
+
   const [stake, setStake] = useState(0);
   const [money, setMoney] = useState(Number(router.query.money));
+  const [betResult, setBetResult] = useState('');
 
   const userName = router.query.name;
 
@@ -14,67 +16,58 @@ const Game = () => {
   
   const rollTheWheel = async () => {
     const randomNumberBetweenZeroAndTwo = Math.floor(Math.random() * 3);
-    console.log(money);
-    console.log(stake);
-    let moneyLeft = null;
+
+    let moneyLeft = money;
     switch(randomNumberBetweenZeroAndTwo) {
       case 0:
-        console.log("Bankrupt!");
-        console.log(money - stake);
+        setBetResult("Bankrupt!");
         moneyLeft = money - stake;
-        try {
-          const res = await fetch(`http://localhost:3000/api/updateMoney`, {
-            method: 'PUT',
-            body: JSON.stringify({
-              data:{name:userName, money:moneyLeft}, 
-            }),
-            headers: {
-              'Content-type': 'application/json; charset=UTF-8',
-            }
-          });
 
-          const data = await res.json();
-          setMoney(data.money);
-        } catch (err) {
-          console.log(err);
-        }
         break;
       case 1:
-        console.log("Keep")
-        console.log(money)
+        setBetResult("Keep");
+
         break;
       case 2:
-        console.log("Double")
-        console.log(money + stake)
+        setBetResult("Double")
         moneyLeft = money + stake;
-        try {
-          const res = await fetch(`http://localhost:3000/api/updateMoney`, {
-            method: 'PUT',
-            body: JSON.stringify({
-              data:{name:userName, money:moneyLeft}, 
-            }),
-            headers: {
-              'Content-type': 'application/json; charset=UTF-8',
-            }
-          });
+    }
 
-          const data = await res.json();
-          setMoney(data.money);
-        } catch (err) {
-          console.log(err);
-        }
+    if (moneyLeft !== null) {
+      router.push({pathname: '/game', query: { name: userName, money: moneyLeft}});
+      await updateMoneyInDatabase(userName, moneyLeft);
     }
   };
 
+  async function updateMoneyInDatabase(userName, moneyLeft) {
+    try {
+      const res = await fetch(`http://localhost:3000/api/updateMoney`, {
+        method: 'PUT',
+        body: JSON.stringify({
+          data: { name: userName, money: moneyLeft },
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        }
+      });
+  
+      const data = await res.json();
+      setMoney(data.money);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <>
-    <p>{`player name: ${name}`}</p>
+    <p>{`player name: ${userName}`}</p>
     <p>{`current money: ${money}`}</p>
     <div>
       <p>Way of betting:</p>
       <input type="number" min="1" max={money} onChange={handleChange}/>
     </div>
     <button onClick={rollTheWheel}>Roll the wheel</button>
+    <p>{betResult}</p>
     </>
   );
 } 
